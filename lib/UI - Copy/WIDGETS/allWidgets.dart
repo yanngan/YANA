de13f5b31768e49f373188093a0dfae9683f35aa
Lidor Eliyahu Shelef <ivanlidor98@gmail.com>
2021-03-25 11:02:50 +0200

    Summary:  - Fixed temp crashing  - Upgraded the bottom navigation bar to be on top of background with a transparent bg instead of it's natural behavior of pushing it up
     # Lidor

--- lib/UI - Copy/WIDGETS/allWidgets.dart
+++
@@ -1,3 +0,0 @@
-/// Developers: Lidor Eliyahu Shelef, Yann Ganem, Yisrael Bar-Or and Jonas Sperling
-
-export 'MyCurvedNavigationBar.dart';
--- lib/UX - Copy/LOGIC/EXCEPTIONS/CanNotGetUserLocationException.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-
-class CanNotGetUserLocationException implements Exception{
-  final String message = "CAN'T GET USER LOCATION!";
-  String toString() {
-    return message;
-  }
-}

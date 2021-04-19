6df44daa42ede912e840d775647cf0a88a0e17b3
Lidor Eliyahu Shelef <ivanlidor98@gmail.com>
2021-04-19 10:29:37 +0300

    Summary:  - Fixed class names  - pptx updated
     # Lidor

--- lib/UI/PAGES/Login.dart
+++ lib/UI/PAGES/Login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -6,11 +8,54 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+
+  static final FacebookLogin facebookSignIn = new FacebookLogin();
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.amber,
-      child: Center(child: Text("Login")),
+      child: Center(
+          child: RaisedButton(
+            child: Text('Login from here!'),
+            onPressed: () async {
+              final FacebookLoginResult result =
+              await facebookSignIn.logIn(['email']);
+              switch (result.status) {
+                case FacebookLoginStatus.loggedIn:
+                  final FacebookAccessToken accessToken = result.accessToken;
+                  print('''
+                     Logged in!
+                     Token: ${accessToken.token}
+                     User id: ${accessToken.userId}
+                     Expires: ${accessToken.expires}
+                     Permissions: ${accessToken.permissions}
+                     Declined permissions: ${accessToken.declinedPermissions}
+                     ''');
+                  break;
+                case FacebookLoginStatus.cancelledByUser:
+                  print('Login cancelled by the user.');
+                  break;
+                case FacebookLoginStatus.error:
+                  Fluttertoast.showToast(
+                      msg: 'Something went wrong with the login process.\n'
+                          'Here\'s the error Facebook gave us: ${result
+                          .errorMessage}',
+                      toastLength: Toast.LENGTH_LONG,
+                      gravity: ToastGravity.CENTER,
+                      timeInSecForIosWeb: 1,
+                      backgroundColor: Colors.red,
+                      textColor: Colors.white,
+                      fontSize: 16.0
+                  );
+                  print('Something went wrong with the login process.\n'
+                      'Here\'s the error Facebook gave us: ${result
+                      .errorMessage}');
+                  break;
+              }
+            }
+        ),
+      )
     );
   }
 }

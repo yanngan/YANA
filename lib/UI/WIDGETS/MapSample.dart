f7b327290aab50d1cc74c27d37be3256b430f281
yanngan <55285071+yanngan@users.noreply.github.com>
2021-05-01 16:32:33 +0300

    1. Create Widget called AddSeeEvent to add + see + edit Events 2. create Event class 3. create Place class 3. create MyMarker class (to be use in the MAP) 4. added in LOGIC MapThing to handle the MAP 5. edited MapView    a. add 3 buttons    b. now map can focus on user current location    c. add few fake point    d. open AddSeeEvent in alertDialog

--- lib/UI/WIDGETS/MapSample.dart
+++ lib/UI/WIDGETS/MapSample.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 import 'package:yana/UX/LOGIC/Logic.dart';
+import 'package:yana/UX/LOGIC/MapThings.dart';
+
+import 'allWidgets.dart';
 
 class MapSample extends StatefulWidget {
   @override
@@ -13,6 +15,7 @@ class MapSample extends StatefulWidget {
 class MapSampleState extends State<MapSample> {
 
   Completer<GoogleMapController> _controller = Completer();
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
@@ -37,17 +40,41 @@ class MapSampleState extends State<MapSample> {
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
+        markers: Set<Marker>.of(markers.values),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 60.0),
-        child: FloatingActionButton.extended(
-          onPressed: jumpToCurrentLocation,
-          backgroundColor: Colors.amber,
-          label: Text('To My Location!', style: TextStyle(fontSize: 20, color: Colors.black87),),
-          icon: Icon(Icons.directions_boat, color: Colors.black54,),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 60),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: jumpToCurrentLocation,
+                  backgroundColor: Colors.amber,
+                  child: Icon(Icons.location_searching, color: Colors.pink,size: 30,),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    FloatingActionButton(
+                      onPressed: addFakePoints,
+                      backgroundColor: Colors.amber,
+                      child: Icon(Icons.ac_unit_sharp, color: Colors.pink,size: 30,),
+                    ),
+                    FloatingActionButton(
+                      onPressed: ()=>addPoints(context),
+                      backgroundColor: Colors.amber,
+                      child: Icon(Icons.add, color: Colors.pink,size: 30,),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
-      ),
     );
   }
 
@@ -64,17 +91,36 @@ class MapSampleState extends State<MapSample> {
   void jumpToCurrentLocation() async {
     CameraPosition CurrntUserLocation = await Logic.getUserLocation();
     _goTo(CurrntUserLocation);
+  }
+  void addPoints(BuildContext context) async{
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: new Text("Add new Place ! 😋",style: TextStyle(color: Colors.amber),),
+          backgroundColor: Colors.pink,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20.0))
+          ),
+          content: AddSeeEvent(),
+          actions: <Widget>[
+            new TextButton(
+              child: new Text("OK",style: TextStyle(color: Colors.amber),),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
-//    Fluttertoast.showToast(
-//        msg: "Location:\n" + CurrntUserLocation.toString(),
-//        toastLength: Toast.LENGTH_LONG,
-//        gravity: ToastGravity.CENTER,
-//        timeInSecForIosWeb: 1,
-//        backgroundColor: Colors.red,
-//        textColor: Colors.white,
-//        fontSize: 16.0
-//    );
-
+  void addFakePoints()async{
+    var res = await MapThings.getMarkers();
+    setState(() {
+      markers = res;
+    });
   }
 
 
---
+++ lib/UX/LOGIC/MapThings.dart
@@ -0,0 +1,25 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:yana/UX/LOGIC/Logic.dart';
+
+import 'EXCEPTIONS/CLASSES/allClasses.dart';
+
+class MapThings{
+
+
+  static getMarkers() async {
+    Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+    CameraPosition CurrntUserLocation = await Logic.getUserLocation();
+    var latitude = CurrntUserLocation.target.latitude;
+    var longitude = CurrntUserLocation.target.longitude;
+    for(int i = 0 ;i < 10 ; i++){
+      String text = "latitude = $latitude longitude = $longitude";
+      Place tempPlace = new Place("$i",i,"name $i",latitude+i,longitude+i);
+      Event tempEvent = new Event(tempPlace,"event id-$i");
+      markers[MarkerId("$i")] = new MyMarker(tempEvent,markerId: MarkerId("$i"),position: LatLng(latitude+i,longitude+i),onTap: (){
+        print(tempEvent.place.name);
+      });
+    }
+    return markers;
+
+  }
+}

e8b53958a732539c16083b12bbab4627b037c29c
yanngan <55285071+yanngan@users.noreply.github.com>
2021-05-02 08:48:46 +0300

    add option to switch map type

--- lib/UI/WIDGETS/MapSample.dart
+++ lib/UI/WIDGETS/MapSample.dart
@@ -16,9 +16,10 @@ class MapSampleState extends State<MapSample> {
 
   Completer<GoogleMapController> _controller = Completer();
   Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  bool mapType = true; // true == MapType.hybrid, false == MapType.normal
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+  static final CameraPosition _TLV = CameraPosition(
+    target: LatLng(32.085300, 34.781769),
     zoom: 14.4746,
   );
 
@@ -26,7 +27,7 @@ class MapSampleState extends State<MapSample> {
       bearing: 192.8334901395799,
       target: LatLng(37.43296265331129, -122.08832357078792),
       tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+      zoom: 100.151926040649414);
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +36,8 @@ class MapSampleState extends State<MapSample> {
       body: GoogleMap(
         zoomControlsEnabled: false,   // False reason -> could'nt figure out how to move it's position from our custom navigation bar
         zoomGesturesEnabled: true,
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
+        mapType: mapType ? MapType.hybrid : MapType.normal,
+        initialCameraPosition: _TLV,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
@@ -51,23 +52,45 @@ class MapSampleState extends State<MapSample> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.end,
               children: <Widget>[
-                FloatingActionButton(
-                  onPressed: jumpToCurrentLocation,
-                  backgroundColor: Colors.amber,
-                  child: Icon(Icons.location_searching, color: Colors.pink,size: 30,),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FloatingActionButton(
+                        onPressed: switchMapType,
+                        backgroundColor: Colors.amber,
+                        child: Icon(mapType?Icons.satellite:Icons.map_outlined, color: Colors.pink,size: 30,),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FloatingActionButton(
+                        onPressed: jumpToCurrentLocation,
+                        backgroundColor: Colors.amber,
+                        child: Icon(Icons.location_searching, color: Colors.pink,size: 30,),
+                      ),
+                    ),
+                  ],
                 ),
                 Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    FloatingActionButton(
-                      onPressed: addFakePoints,
-                      backgroundColor: Colors.amber,
-                      child: Icon(Icons.ac_unit_sharp, color: Colors.pink,size: 30,),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FloatingActionButton(
+                        onPressed: addFakePoints,
+                        backgroundColor: Colors.amber,
+                        child: Icon(Icons.ac_unit_sharp, color: Colors.pink,size: 30,),
+                      ),
                     ),
-                    FloatingActionButton(
-                      onPressed: ()=>addPoints(context),
-                      backgroundColor: Colors.amber,
-                      child: Icon(Icons.add, color: Colors.pink,size: 30,),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FloatingActionButton(
+                        onPressed: ()=>addPoints(context),
+                        backgroundColor: Colors.amber,
+                        child: Icon(Icons.add, color: Colors.pink,size: 30,),
+                      ),
                     ),
                   ],
                 )
@@ -118,11 +141,14 @@ class MapSampleState extends State<MapSample> {
 
   void addFakePoints()async{
     var res = await MapThings.getMarkers();
-    setState(() {
-      markers = res;
-    });
+
   }
 
+  void switchMapType(){
+    setState(() {
+      mapType = !mapType;
+    });
+  }
 
 
 
